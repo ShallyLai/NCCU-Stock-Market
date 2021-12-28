1f2fdfb4624f4bbd39f0da9e96c7988e67f01626
wutever0017 <elizabethlin77@gmail.com>
2021-12-28 15:45:27 +0800

    port

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,6 @@ app.post('/login', function(req, res){
     });
 })
 
-var server = app.listen(3001, function() {
-    console.log('Listening on port 3001');
+var server = app.listen(3000, function() {
+    console.log('Listening on port 3000');
 });
