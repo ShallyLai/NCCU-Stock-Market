2f64a40c92375c744ae19fdb3759f40139a3d48c
wutever0017 <elizabethlin77@gmail.com>
2022-01-04 15:15:19 +0800

    0104

--- backend/getAllStock.js
+++ backend/getAllStock.js
@@ -6,56 +6,25 @@ let handle_request = async((data, callback) => {
   let response = { status: 400 };
 
   try {
+
+    var price = [];
+    var name = [];
+    var id = [];
+    var high = [];
+    var low = [];
+
     let query = "select price, company_name, company_id from Stock, Company where Stock.scompany_id=Company.company_id;";
 
     mysql.fetchData(query, function (err, result) {
       if (err) {
         throw (err);
       } else {
-        // result: price, name, id
-        var price = [];
-        var name = [];
-        var id = [];
-        var high = [];
-        var low = [];
-
         for (i = 0; i < result.length; i++) {
           price.push(result[i].price);
           name.push(result[i].company_name);
           id.push(result[i].company_id);
-
         }
-
-        const high_low = result.price.map(async(element, index) => {
-          console.log("index:", index);
-          console.log("element:", element);
-
-          let table_name = "History_" + result[i].company_id;
-          let query2 = "select price from " + table_name + " where Date_=curDate();";
-          // select * from History_101 order by Date_ DESC, Time_ DESC limit 1;
-          // if query2.length >= 1, find high low
-
-          // not query2.length == 0, get current price
-          mysql.fetchData(query2, function (err2, result2) {
-            if (err2) {
-              throw (err2);
-            } else if (result2.length == 0) {
-              console.log("no curDate() price");
-              high.push(result[i].price);
-              low.push(result[i].price);
-            } else {
-              high.push(Math.max(...result2));
-              low.push(Math.min(...result2));
-            }
-          });
-        })
-        await Promise.all(high_low).then(() => {
-          console.log("awaited all promises");
-        })
-
-
         // for (i = 0; i < result.length; i++) {
-
         //   let table_name = "History_" + result[i].company_id;
         //   let query2 = "select price from " + table_name + " where Date_=curDate();";
         //   // select * from History_101 order by Date_ DESC, Time_ DESC limit 1;
@@ -74,20 +43,45 @@ let handle_request = async((data, callback) => {
         //       low.push(Math.min(...result2));
         //     }
         //   });
-
         // }
-
-        response.status = 200;
-        response.price = price;
-        response.name = name;
-        response.id = id;
-        response.high = high;
-        response.low = low;
-
-        callback(null, response);
-
       }
-    })
+    });
+
+    const high_low = result.price.map(async (element, index) => {
+      console.log("index:", index);
+      console.log("element:", element);
+
+      let table_name = "History_" + result[i].company_id;
+      let query2 = "select price from " + table_name + " where Date_=curDate();";
+      // select * from History_101 order by Date_ DESC, Time_ DESC limit 1;
+      // if query2.length >= 1, find high low
+      // not query2.length == 0, get current price
+      mysql.fetchData(query2, function (err2, result2) {
+        if (err2) {
+          throw (err2);
+        } else if (result2.length == 0) {
+          console.log("no curDate() price");
+          high.push(result[i].price);
+          low.push(result[i].price);
+        } else {
+          high.push(Math.max(...result2));
+          low.push(Math.min(...result2));
+        }
+      });
+    });
+    await Promise.all(high_low).then(() => {
+      console.log("awaited all promises");
+    });
+
+    response.status = 200;
+    response.price = price;
+    response.name = name;
+    response.id = id;
+    response.high = high;
+    response.low = low;
+
+    callback(null, response);
+
   } catch (err) {
     console.log("catch error");
     callback(err, response);
