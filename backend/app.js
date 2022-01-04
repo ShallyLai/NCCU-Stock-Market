bd9febc313dd70269bb8fc3386352b95eed5e1a3
ShallyLai <79987875+ShallyLai@users.noreply.github.com>
2022-01-04 14:24:44 +0800

    getStock

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cp());
 app.use(cors());
 
-let signup = require('./signup.js');
-let login = require('./login.js');
-let getAllStock = require('./getAllStock.js');
+let signup = require('./backend/signup.js');
+let login = require('./backend/login.js');
+let allStock = require('./backend/getALLStock.js');
 
 app.get('/', function(req, res){
     console.log("connected to port 3000");
     res.send("connected to port 3000");
+    //res.sendfile('./views/index.html');
 });
 app.get('/test', function(req, res){
     console.log("test success");
@@ -49,15 +50,27 @@ app.post('/login', function(req, res){
             res.send(result).status(result.status);
         }
     });
-});
+})
 
-app.get('/get_all_stock', function(req, res){
-    console.log('get all stock');
-
-    getAllStock.handle_request(function(err, result){
-
+app.get('/GetALLStock', function(reg, res){
+    console.log("get all stocks");
+    console.log("req.body");
+    console.log(req.body);
+    allStock.handle_request(req.body, function(err, result){
+        if(err){
+            console.log("get all Stock handle_request error");
+            res.send(result).status(result.status);
+        } else {
+            res.send(result).status(result.status);
+        }
     });
 })
+
+app.post("/delete",function(req,res){
+    console.log('post login');
+    console.log('req.body:');
+    
+});
 
 var server = app.listen(3000, function() {
     console.log('Listening on port 3000');
--- backend/getAllStock.js
+++ backend/getAllStock.js
@@ -1,6 +1,6 @@
 // 這裡的路徑每個人可能不太一樣
 // 看你接SQL的檔案在哪裡
-var mysql = require('../my/mysql.js');
+var mysql = require('/Users/shallylai/Desktop/DBMS/FinalProject/mysql/mysql.js');
 
 handle_request = ((callback) => {
     let response = { status:400};
@@ -12,23 +12,59 @@ handle_request = ((callback) => {
             if(err){
                 throw(err);
             } else {
-                // result: 
+                // result: price, name, id
+                var price = [];
+                var name = [];
+                var id = [];
+                var high = [];
+                var low = [];
+
                 console.log(result);
+
+                for (i = 0; i<result.length; i++){
+                  price.push(result[i].price);
+                  name.push(result[i].company_name);
+                  id.push(result[i].company_id);
+                }
+
 
                 for (i = 0; i < result.length; i++){
 
                     let table_name = "History_" + result[i].company_id;
-                    let query2 = "select * from" + table_name +  "where Date_=curDate();";
+                    let query2 = "select price from" + table_name +  "where Date_=curDate();";
                     // select * from History_101 order by Date_ DESC, Time_ DESC limit 1;
                     // if query2.length >= 1, find high low
 
                     // not query2.length == 0, get current price
+                    mysql.fetchData(query2, function(err2, result2){
+                      if(err2){
+                        throw(err2);
+                      } else if(result2.length == 0){
+                        high.push(result[i].price);
+                        low.push(result[i].price);
+                      } else{
+                        //let maxQuery = "select max(price) from " + table_name + " where Date_=curDate();";
+                        //let minQuery = "select min(price) from " + table_name + " where Date_=curDate();";
+                        high.push(Math.max(...result2));
+                        low.push(Math.min(...result2));
+                      }
+                    });
+
                 }
-                
+
+                response.status = 200;
+                response.price = price;
+                response.name = name;
+                response.id = id;
+                response.high = high;
+                response.low = low;
+
+                callback(null, response);
 
             }
         })
     } catch(err) {
         console.log("catch error");
+        callback(err, response);
     }
 })
