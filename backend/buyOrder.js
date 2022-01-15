45473e960e1019065f3c59b9d00e71d3a45f8c5b
wutever0017 <elizabethlin77@gmail.com>
2022-01-15 16:49:22 +0800

    buyOrder

--- backend/buyOrder.js
+++ backend/buyOrder.js
@@ -23,6 +23,19 @@ handle_request = async(data, callback) => {
       }
     });
 
+    let orderID = 0;
+    let getorderid = "select order_id from BuyOrder order by time desc limit 1;";
+    await mysql.myFetch(getorderid, function(err, getid_res){
+      if(err){
+        console.log("get order id err");
+        throw err;
+      } else if(getid_res.length == 1){
+        console.log("get id");
+        console.log(getid_res);
+        orderID = getid_res[0][0];
+      }
+    });
+
     // 漲價
     let priceChange = Math.floor(data.num/10) + 1;
     let oldprice = 0;
@@ -35,7 +48,9 @@ handle_request = async(data, callback) => {
       } else if(get_res.length == 1){
         console.log("get price:");
         console.log(get_res);
-        oldprice = get_res[0].price;
+        console.log(get_res[0]);
+        console.log(get_res[0][0]);
+        oldprice = get_res[0][0];
       }
     });
 
@@ -48,12 +63,33 @@ handle_request = async(data, callback) => {
       if(err){
         console.log("update price err");
         throw err;
-      } else if(update_res.length == 1){
+      } else if(update_res.affectedRows == 1){
         console.log("update price:");
       }
     });
 
-    
+    let matchq = "select SellOrder.order_id, SellOrder.num from BuyOrder, SellOrder, Stock where BuyOrder.order_id=" + 
+                  orderID + " and SellOrder.stock_id=BuyOrder.stock_id and Stock.stock_id=BuyOrder.stock_id and " + 
+                  "BuyOrder.num != 0 and SellOrder.num != 0 and " + 
+                  "SellOrder.price <= Stock.price and Stock.price <= BuyOrder.price " + 
+                  "order by SellOrder.time;";
+
+    await mysql.myFetch(matchq, function(err, match_res){
+      if(err){
+        console.log("match err");
+        throw err;
+      } else if(match_res.length >= 1){
+        console.log("matched");
+        console.log(match_res);
+      } else {
+        console.log("no match");
+        response.msg = "no match";
+        response.status = 404;
+        callback(null, response);
+      }
+    });
+
+
 
   } catch (err) {
     console.log(err);
