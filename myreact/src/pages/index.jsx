ef871f38a0c73584eff780437b15ae33324eff08
aoi-mitori <hs410268nu@gmail.com>
2022-01-18 01:22:14 +0800

    some login and get stock

--- myreact/src/pages/index.jsx
+++ myreact/src/pages/index.jsx
@@ -19,13 +19,31 @@ const loginSummit = async (acc) => {
     }
     const res = await fetch(
       'http://localhost:3000/login', {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: new Headers({
-          'Content-Type': 'application/json'
-        })
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }
+    ).then((response) => {
+  
+      console.log(response.status);
+      return response.json();
+    }).then((response_json) => {
+      if (response_json.msg === 'no user found') {
+        alert('帳號錯誤');
+        return;
+      }
+      else if (response_json.msg === 'password error') {
+        alert('密碼錯誤');
+        return;
+      }
+      else if (response_json.msg === 'found user') {
+        alert('登入成功');
+        sessionStorage.setItem('user', acc.acc);
+        return;
+      }
+  
     });
   }
-
-
-export default LoginPage;
+  export default LoginPage;
--- myreact/src/pages/stock.jsx
+++ myreact/src/pages/stock.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import Button from './component/Button';
+
 
 const StockPage = () => {
     return (
         <div>
             <h3>StockPage</h3>
+            <Button color='blue' text='get_stock' onClick = {getStock}/>
         </div>
     );
 };
+
+const getStock = async () => {
+
+    const res = await fetch(
+      'http://localhost:3000/getAllStock', {
+      method: "GET",
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }
+    ).then((response) => {
+      console.log(response.status);
+      return response.json();
+    }).then((response_json) => {
+        console.log(response_json);
+    //   if (response_json.msg === 'no user found') {
+    //     alert('帳號錯誤');
+    //     return;
+    //   }
+    //   else if (response_json.msg === 'password error') {
+    //     alert('密碼錯誤');
+    //     return;
+    //   }
+    //   else if (response_json.msg === 'found user') {
+    //     alert('登入成功');
+    //     sessionStorage.setItem('user', acc.acc);
+    //     return;
+    //   }
+  
+    });
+  }
 
 export default StockPage;
