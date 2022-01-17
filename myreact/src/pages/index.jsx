40fb59e6e3ce0f8a60ec1618e6dc6553b472e59c
aoi-mitori <hs410268nu@gmail.com>
2022-01-17 23:59:58 +0800

    move /component into /pages

--- myreact/src/pages/index.jsx
+++ myreact/src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from '../component/Header';
-import Login from '../component/Login';
+import Header from './component/Header';
+import Login from './component/Login';
 
 const LoginPage = () => {
     return (
