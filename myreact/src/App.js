1ca3b6ce9f2298c9303bd4b160bc467c05bb0596
aoi-mitori <hs410268nu@gmail.com>
2022-01-18 10:42:59 +0800

    stock table

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -14,6 +14,7 @@ import LoginPage from "./pages/index";
 import NotFoundPage from './pages/404';
 import HistoryPage from './pages/history';
 import StockPage from './pages/stock';
+import TestPage from './pages/test_table';
 
 class App extends React.Component {
   render() {
@@ -23,7 +24,7 @@ class App extends React.Component {
           <Route path="/" element={<LoginPage/>}></Route>
           <Route path="/history" element={<HistoryPage/>} ></Route>
           <Route path="/stock" element={<StockPage/>} ></Route>
-          <Route path="/404" element={<NotFoundPage/>} ></Route>
+          <Route path="/test" element={<TestPage/>} ></Route>
           <Route path="/404" element={<NotFoundPage/>} ></Route>
           <Route path="*" element={<Navigate to ="/404" />}/> 
         </Routes>
--- myreact/src/pages/stock.jsx
+++ myreact/src/pages/stock.jsx
@@ -1,45 +1,51 @@
+import { useState, useEffect } from 'react'
 import React from "react";
 import Button from './component/Button';
-
+import EnhancedTable from "./test_table";
 
 const StockPage = () => {
+    const [allStocks, setAllStocks] = useState([]);
+    useEffect(() => {
+        const getAllStocks = async () => {
+          const allStocksFromServer = await fetchAllStocks()
+          setAllStocks(allStocksFromServer)
+        }
+        getAllStocks()
+    }, [])
+
+    //Fetch All Stocks
+    const fetchAllStocks = async () => {
+        const res = await fetch('http://localhost:3000/getAllStock')
+        const data = await res.json()
+        return data
+    }
+    
+    //console.log(allStocks.name)
     return (
         <div>
             <h3>StockPage</h3>
-            <Button color='blue' text='get_stock' onClick = {getStock}/>
+            {/* <Button color='blue' text='get_stock' onClick = {getStock}/> */}
+            <EnhancedTable data={allStocks}/>
         </div>
     );
 };
 
-const getStock = async () => {
+// const getStock = async () => {
+//     const res = await fetch(
+//       'http://localhost:3000/getAllStock', {
+//       method: "GET",
+//       headers: new Headers({
+//         'Content-Type': 'application/json'
+//       })
+//     }
+//     ).then((response) => {
+//       console.log(response.status);
+//       return response.json();
+//     }).then((response_json) => {
+//         console.log(response_json.name[0]);
+//     });
+// }
 
-    const res = await fetch(
-      'http://localhost:3000/getAllStock', {
-      method: "GET",
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      })
-    }
-    ).then((response) => {
-      console.log(response.status);
-      return response.json();
-    }).then((response_json) => {
-        console.log(response_json);
-    //   if (response_json.msg === 'no user found') {
-    //     alert('帳號錯誤');
-    //     return;
-    //   }
-    //   else if (response_json.msg === 'password error') {
-    //     alert('密碼錯誤');
-    //     return;
-    //   }
-    //   else if (response_json.msg === 'found user') {
-    //     alert('登入成功');
-    //     sessionStorage.setItem('user', acc.acc);
-    //     return;
-    //   }
-  
-    });
-  }
+
 
 export default StockPage;
