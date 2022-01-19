09ce363b00eab875256a1fddfc969343ec01da1e
ShallyLai <79987875+ShallyLai@users.noreply.github.com>
2022-01-19 13:36:03 +0800

    getMyOrder_add_cname

--- backend/getMyOrder.js
+++ backend/getMyOrder.js
@@ -8,21 +8,27 @@ let handle_request = async (user, callback) => {
   var buy_num = [];
   var buy_price = [];
   var buy_time = [];
+  var buy_name = [];
 
   var sell_order_id = [];
   var sell_stock_id = [];
   var sell_num = [];
   var sell_price = [];
   var sell_time = [];
+  var sell_name = [];
 
   try {
-    let buyQuery = "select * from BuyOrder where buser_id='" + user.user_id + "' and Date(time)=curDate();";
+    // get buy order history
+    var buy_length = 0;
+    let buyQuery = "select * from BuyOrder where buser_id=" + user.user_id + " and Date(time)=curDate();";
     await mysql.myFetch(buyQuery, function (err, result) {
       if (err) {
         console.log(err);
         throw "select buy order error";
       } else {
         for(i = 0; i < result.length; i++){
+          buy_length = result.length;
+
           buy_order_id.push(result[i][0]);
           buy_stock_id.push(result[i][1]);
           buy_num.push(result[i][2]);
@@ -32,12 +38,28 @@ let handle_request = async (user, callback) => {
       }
     });
 
-    let sellQuery = "select * from SellOrder where suser_id='" + user.user_id + "' and Date(time)=curDate();";
+    for(var a=0;a<buy_length;a++){
+      let buyCompany = "select company_name from Company where company_id = " + buy_stock_id[a] + ";";
+      await mysql.myFetch(buyCompany, function (error, c_name) {
+        if(error){
+          console.log(error);
+          throw "No company";
+        } else{
+          console.log("buy company name:");
+          console.log(c_name[0][0]);
+          buy_name.push(c_name[0][0]);
+        }
+      });
+    }
+    // get sell order history
+    var sell_length = 0;
+    let sellQuery = "select * from SellOrder where suser_id=" + user.user_id + " and Date(time)=curDate();";
     await mysql.myFetch(sellQuery, function (err, result) {
       if (err) {
         console.log(err);
         throw "select sell order error";
       } else {
+        sell_length = result.length;
         for(i = 0; i < result.length; i++){
           sell_order_id.push(result[i][0]);
           sell_stock_id.push(result[i][1]);
@@ -47,6 +69,19 @@ let handle_request = async (user, callback) => {
         }
       }
     });
+    for(var a=0;a<sell_length;a++){
+      let sellCompany = "select company_name from Company where company_id = " + sell_stock_id[a] + ";";
+      await mysql.myFetch(sellCompany, function (error, c_name) {
+        if(error){
+          console.log(error);
+          throw "No company";
+        } else{
+          console.log("sell company name:");
+          console.log(c_name[0][0]);
+          sell_name.push(c_name[0][0]);
+        }
+      });
+    }
 
     if(buy_price.length == 0 && sell_price.length == 0 ){
       response.msg = 'no order';
@@ -58,11 +93,14 @@ let handle_request = async (user, callback) => {
 
     response.buy_order_id = buy_order_id;
     response.buy_stock_id = buy_stock_id;
+    response.buy_name = buy_name;
     response.buy_num = buy_num;
     response.buy_price = buy_price;
     response.buy_time = buy_time;
+    
     response.sell_order_id = sell_order_id;
     response.sell_stock_id = sell_stock_id;
+    response.sell_name = sell_name;
     response.sell_num = sell_num;
     response.sell_price = sell_price;
     response.sell_time = sell_time;
