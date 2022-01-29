6b40b3a7668e09905aae856bd32d36933e7593e0
ShallyLai <79987875+ShallyLai@users.noreply.github.com>
2022-01-29 14:02:34 +0800

    update_signupAPI

--- backend/getMyTransaction.js
+++ backend/getMyTransaction.js
@@ -1,75 +1,76 @@
 var mysql = require('../mysql/mysql.js');
 
 handle_request = (async(data, callback) => {
-    let response = {status: 400};
-    try{
-			var stock_name = []; 
-			var BuyOrSell = [];
-			var TransactionPrice = [];
-			var num = [];
-			var TransactionTime = [];
+  let response = {status: 400};
+  
+  try{
+		var stock_name = []; 
+		var BuyOrSell = [];
+		var TransactionPrice = [];
+		var num = [];
+		var TransactionTime = [];
 
-    	let get_Buy_trans = "select company_name, MyTransaction.price, MyTransaction.num, finish_time from User, BuyOrder, MyTransaction, Company " + 
-    	"where " + data.user_id + " = user_id AND "+ "user_id = buser_id AND order_id = tbuy_order_id AND tstock_id = company_id;";
- 			let get_Sell_trans = "select company_name, MyTransaction.price, MyTransaction.num, finish_time from User, SellOrder, MyTransaction, Company " + 
-    	"where " + data.user_id + " = user_id AND "+ "user_id = suser_id AND order_id = tsell_order_id AND tstock_id = company_id;";
+  	let get_Buy_trans = "select company_name, MyTransaction.price, MyTransaction.num, finish_time from User, BuyOrder, MyTransaction, Company " + 
+  	"where " + data.user_id + " = user_id AND "+ "user_id = buser_id AND order_id = tbuy_order_id AND tstock_id = company_id;";
+			let get_Sell_trans = "select company_name, MyTransaction.price, MyTransaction.num, finish_time from User, SellOrder, MyTransaction, Company " + 
+  	"where " + data.user_id + " = user_id AND "+ "user_id = suser_id AND order_id = tsell_order_id AND tstock_id = company_id;";
 
-	    await mysql.myFetch(get_Buy_trans, function(err, result){
-  	  	if(err){
-    			console.log("error");
-    			throw(err);
-    		} else {
-    			console.log("Buy Transaction Get ");
-    			for(a=0;a<result.length;a++){
-    				stock_name.push(result[a][0]);
-    				BuyOrSell.push("TRUE");
-    				TransactionPrice.push(result[a][1]);
-    				num.push(result[a][2]);
-    				TransactionTime.push((result[a][3]));
-    			}
-    		}	
+    await mysql.myFetch(get_Buy_trans, function(err, result){
+	  	if(err){
+  			console.log("error");
+  			throw(err);
+  		} else {
+  			console.log("Buy Transaction Get ");
+  			for(a=0;a<result.length;a++){
+  				stock_name.push(result[a][0]);
+  				BuyOrSell.push("TRUE");
+  				TransactionPrice.push(result[a][1]);
+  				num.push(result[a][2]);
+  				TransactionTime.push((result[a][3]));
+  			}
+  		}	
 
-    	});
-    	await mysql.myFetch(get_Sell_trans, function(err, result){
-  	  	if(err){
-    			console.log("error");
-    			throw(err);
-    		} else {
-    			console.log("Sell Transaction Get ");
-    			for(a=0;a<result.length;a++){
-    				stock_name.push(result[a][0]);
-    				BuyOrSell.push("FALSE");
-    				TransactionPrice.push(result[a][1]);
-    				num.push(result[a][2]);
-    				TransactionTime.push(result[a][3]);
-    			}
-    		}	
+  	});
+  	await mysql.myFetch(get_Sell_trans, function(err, result){
+	  	if(err){
+  			console.log("error");
+  			throw(err);
+  		} else {
+  			console.log("Sell Transaction Get ");
+  			for(a=0;a<result.length;a++){
+  				stock_name.push(result[a][0]);
+  				BuyOrSell.push("FALSE");
+  				TransactionPrice.push(result[a][1]);
+  				num.push(result[a][2]);
+  				TransactionTime.push(result[a][3]);
+  			}
+  		}	
 
-    	});
-			if(TransactionPrice.length == 0){
-				response.msg = 'no transaction';
-				response.status = 204;
-			} else {
-				response.msg = "get transaction";
-				response.status = 200;
-			}
-    	response.stock_name = stock_name;
-    	response.BuyOrSell = BuyOrSell;
-    	response.TransactionPrice = TransactionPrice;
-    	response.num = num;
-    	response.TransactionTime = TransactionTime;
+  	});
+		if(TransactionPrice.length == 0){
+			response.msg = 'no transaction';
+			response.status = 204;
+		} else {
+			response.msg = "get transaction";
+			response.status = 200;
+		}
+  	response.stock_name = stock_name;
+  	response.BuyOrSell = BuyOrSell;
+  	response.TransactionPrice = TransactionPrice;
+  	response.num = num;
+  	response.TransactionTime = TransactionTime;
 
-			console.log("callback response");
-			console.log(response);
+		console.log("callback response");
+		console.log(response);
 
-    	callback(null, response);
-    } catch(err){
-    	console.log("catch");
-    	response.status = 400;
-    	response.msg = "catch error";
-    	console.log (err);
-    	callback(err, response);
-    }
+  	callback(null, response);
+  } catch(err){
+  	console.log("catch");
+  	response.status = 400;
+  	response.msg = "catch error";
+  	console.log (err);
+  	callback(err, response);
+  }
     
  });
 
--- backend/signup.js
+++ backend/signup.js
@@ -1,54 +1,52 @@
 var mysql = require('../mysql/mysql.js');
 
 handle_request = ((data, callback) => {
-    let response = {
-        status: 400
-    };
+  let response = {status: 400};
 
-    try {
-        let checkUser = "select * from User where user_name='" + data.user_name + "';";
-        mysql.fetchData(checkUser, function (err, fetch_res) {
-            if (err) {
-                console.log("err");
-                response.status = 400;
-                response.msg = 'check user_name error';
-                callback(err, response);
-            } else if (fetch_res.length == 0) {
-                let insertUser = "insert into User (user_name, password, money) values ('" +
-                    //data.user_id + "', '" +
-                    data.user_name + "', '" +
-                    data.password + "', '" +
-                    data.money + "');";
+  try {
+    // 註冊就送一萬元
+    let checkUser = "select * from User where user_name='" + data.user_name + "';";
+    mysql.fetchData(checkUser, function (err, fetch_res) {
+      if (err) {
+        console.log("err");
+        response.status = 400;
+        response.msg = 'check user_name error';
+        callback(err, response);
+      } else if (fetch_res.length == 0) {
+        let insertUser = "insert into User (user_name, password, money) values ('" +
+          data.user_name + "', " + data.password + ", 10000);";
 
-                mysql.insertData(insertUser, function (insert_err, insert_res) {
-                    if (insert_err) {
-                        console.log("err");
-                        response.msg = 'insert error';
-                        response.status = 400;
-                        callback(err, response);
-                    } else if (insert_res.affectedRows == 1) {
-                        response.status = 200;
-                        response.msg = "signup success";
-                        console.log("insert response 200");
-                        callback(null, response);
-                    } else {
-                        response.status = 400;
-                        response.msg = 'signup error';
-                        console.log("insert response 201");
-                        callback(null, response);
-                    }
-                });
-            } else {
-                response.status = 403;
-                response.msg = 'user name already used';
-                console.log("user name used");
-                callback(null, response);
-            }
-        })
-    } catch (e) {
-        console.log("catch");
-        callback(e, response);
-    }
+        mysql.insertData(insertUser, function (insert_err, insert_res) {
+          if (insert_err) {
+            console.log("err");
+            response.msg = 'insert error';
+            response.status = 400;
+            callback(err, response);
+          } else if (insert_res.affectedRows == 1) {
+            response.status = 200;
+            response.msg = "signup success";
+            console.log("insert response 200");
+            callback(null, response);
+          } else {
+            response.status = 400;
+            response.msg = 'signup error';
+            console.log("insert response 201");
+            callback(null, response);
+          }
+        });
+      } else {
+        response.status = 403;
+        response.msg = 'user name already used';
+        console.log("user name used");
+        callback(null, response);
+      }
+    })
+  } catch (e) {
+    console.log("catch");
+    response.status = 400;
+    response.msg = "catch error";
+    callback(e, response);
+  }
 });
 
 exports.handle_request = handle_request;
